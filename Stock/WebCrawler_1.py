fdea86de2d75ed15f73a3eb67d5fb1ea2b9476d7
Yang <sky14785214@gmail.com>
2020-08-07 02:38:03 +0800

    新增網路爬蟲EPS

---
+++ Stock/WebCrawler_1.py
@@ -0,0 +1,32 @@
+import urllib.request as req
+url="https://histock.tw/stock/financial.aspx?no=2887&t=2"
+
+#附加headers 偽裝一般使用者
+request = req.Request(url, headers={
+    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"
+})
+
+with req.urlopen(request) as response:
+    data = response.read().decode("utf-8")
+# print(data)
+#解析原始碼
+import bs4
+root = bs4.BeautifulSoup(data,"html.parser")
+
+EPS_5Y = []
+
+def eps(url_data,eps5y):
+    EPS_bf4 = []
+    titles = root.find_all("td",class_="b-b") # 尋找所有 class=title 的div標
+    EPS_bf4.append(titles[2])
+    EPS_bf4.append(titles[7])
+    EPS_bf4.append(titles[12])
+    EPS_bf4.append(titles[17])
+    EPS_bf4.append(titles[22])
+    EPS_bf4.append(titles[27])
+    EPS_bf4.append(titles[32])
+    for i in EPS_bf4:
+        EPS_5Y.append(i.string)
+
+eps(data,EPS_5Y)
+print(EPS_5Y) 
